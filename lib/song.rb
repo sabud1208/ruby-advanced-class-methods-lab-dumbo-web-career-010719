46b889fc61eeb2e571a69814ecfcef161d706e43
Stephanie Abud <sabud1208@gmail.com>
2019-01-15 03:28:01 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+    
 
   def self.all
     @@all
@@ -9,5 +13,69 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+  song = Song.new
+  song.save
+  song
+  end
+
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name 
+    song
+  end
+  
+  
+  def self.find_by_name(song_name)
+    @@all.find do |song|
+   song.name == song_name
+    end
+end
+
+def self.find_or_create_by_name(song_name)
+ self.find_by_name(song_name) || self.create_by_name(song_name)
+end
+
+def self.alphabetical
+  self.all.sort_by do |a|
+  a.name
+ end
+end
+  
+  
+def self.new_from_filename(filename)
+    title = filename.split(" - ")
+    artist_name = title[0]
+    song_name = title[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    title = filename.split(" - ")
+    artist_name = title[0]
+    song_name = title[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song.save
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 
 end
