4b721f025829ad06be077a95bb6f7635e57d9033
Stephanie Abud <sabud1208@gmail.com>
2019-01-15 03:23:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,8 +56,6 @@ def self.new_from_filename(filename)
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
 
-
-    self.create_from_filename(filename)
     song = self.create
     song.name = song_name
     song.artist_name = artist_name
