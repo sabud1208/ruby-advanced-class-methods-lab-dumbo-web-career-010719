b2c79eceac24e967a8f61e8043880ef49e0430ea
Stephanie Abud <sabud1208@gmail.com>
2019-01-15 03:25:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,15 +52,19 @@ end
   
   
 def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
+    title = filename.split(" - ")
+    artist_name = title[0]
+    song_name = title[1].gsub(".mp3", "")
 
     song = self.create
     song.name = song_name
     song.artist_name = artist_name
     song
   end
+  
+  def self.create_from_filename(filename)
+  
+end
 
   def self.destroy_all
     self.all.clear
