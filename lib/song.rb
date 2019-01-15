d2633fb2acdb05c8db6b0126eb009668ddcc3d23
Stephanie Abud <sabud1208@gmail.com>
2019-01-15 02:54:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,10 @@ require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ def initialize(name, artist_name)
+   @name = name
+   @artist_name
+ end
 
   def self.all
     @@all
