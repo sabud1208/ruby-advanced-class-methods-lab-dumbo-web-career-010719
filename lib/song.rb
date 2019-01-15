9e2fe960b1dadc9f3086814aaad6b8d914684d0a
Stephanie Abud <sabud1208@gmail.com>
2019-01-15 02:59:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,9 @@ def self.new_from_filename(filename)
    binding.pry
  song = self.name
  song.name= (filename.split[0])
- song.artist_name(filename.split[2].chomp(Come.mp3))
+ song.artist_name(filename.split[1].chomp(Come.mp3))
+ @@all << song
+ song
 
 end
 
